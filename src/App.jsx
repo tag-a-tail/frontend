f991c22345b14af3c7323447bea93e9b6db86dc8
suraj-mandal <suraj.mandal738@gmail.com>
2024-08-26 23:55:52 +0530

    Added shadcn components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import {Button} from "@/components/ui/button.jsx";
+
 function App() {
-  return (
-      <h1 className="text-3xl font-bold underline">
-          Hello World!
-      </h1>
-  )
+    return (
+        <div className="p-2">
+            <h1 className="text-3xl font-bold underline">
+                Hello World!
+            </h1>
+            <div>
+                <Button>Click Me</Button>
+            </div>
+        </div>
+    )
 }
 
 export default App
